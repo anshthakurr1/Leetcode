ef22bba9382f0a9bd81003de598f33f7b6d5c331
Ansh Thakur <ansh.27240@ggnindia.dronacharya.info>
2026-07-11 00:32:11 +0530

    Time: 60 ms (12.52%) | Memory: 19.1 MB (26.41%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.c
@@ -0,0 +1,25 @@
+bool containsDuplicate(int* nums, int numsSize) {
+    int compare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
+}
+{
+    if (numsSize <= 1)
+    {
+        return false;
+    }
+
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    for (int i = 1; i < numsSize; i++)
+    {
+        if (nums[i] == nums[i - 1])
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+    
+}
