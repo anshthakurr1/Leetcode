2443482f51a2a534020d231631f2300a5e8c504f
Ansh Thakur <ansh.27240@ggnindia.dronacharya.info>
2026-07-12 00:44:50 +0530

    Time: 4 ms (80.15%) | Memory: 11.5 MB (95.08%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.c
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    struct ListNode *ptr=node->next;
+    node->val=ptr->val;
+    node->next=ptr->next;
+
+    free(ptr);
+}
