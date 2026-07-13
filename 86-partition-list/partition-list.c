e5db5f2434ce78b3188e98c80108660122334794
Ansh Thakur <ansh.27240@ggnindia.dronacharya.info>
2026-07-14 00:04:53 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (26.21%) - LeetSync

--- 86-partition-list/partition-list.c
+++ 86-partition-list/partition-list.c
@@ -6,26 +6,28 @@
  * };
  */
 struct ListNode* partition(struct ListNode* head, int x) {
-    struct ListNode smallDummy, largeDummy;
-    smallDummy.next = NULL;
-    largeDummy.next = NULL;
-
-    struct ListNode *small = &smallDummy;
-    struct ListNode *large = &largeDummy;
-
-    while (head != NULL) {
-        if (head->val < x) {
-            small->next = head;
-            small = small->next;
-        } else {
-            large->next = head;
-            large = large->next;
+    struct ListNode largeDummy;
+    largeDummy.next=NULL;
+    struct ListNode smallDummy;
+    smallDummy.next=NULL;
+    struct ListNode *large=&largeDummy;
+    struct ListNode *small=&smallDummy;
+    while(head!=NULL)
+    {
+        if(head->val<x)
+        {
+            small->next=head;
+            small=small->next;
         }
-        head = head->next;
+        else
+        {
+            large->next=head;
+            large=large->next;
+        }
+        head=head->next;
     }
-
-    large->next = NULL;
-    small->next = largeDummy.next;
-
-    return smallDummy.next;
+    large->next=NULL;
+    small->next=largeDummy.next;
+    
+    return smallDummy.next; 
 }
