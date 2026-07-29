cd89322b8f1a5e1f9aa5bc6aeaeb3f5ae6345260
Ansh Thakur <ansh.27240@ggnindia.dronacharya.info>
2026-07-29 22:35:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.64%) - LeetSync

---
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.c
@@ -0,0 +1,23 @@
+int getLucky(char* s, int k) {
+    int sum=0;
+    for(int i=0;s[i]!='\0';i++)
+    {
+        int val=s[i]-'a'+1;
+     while(val>0)
+     {
+        sum+=val%10;
+        val/=10;
+     }
+    }
+     while(--k)
+     {
+        int temp=0;
+        while(sum>0)
+        {
+            temp+=sum%10;
+            sum/=10;
+        }
+        sum=temp;
+     }
+     return sum;
+}
