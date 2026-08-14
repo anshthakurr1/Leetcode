c92a337f5229cacc8f7d60991afd05586ea3f8d5
Ansh Thakur <ansh.27240@ggnindia.dronacharya.info>
2026-08-14 23:24:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (70.59%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,18 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    if(numsSize==0)
+    {
+        return 0;
+    }
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]==target)
+        {
+            return i;
+        }
+        if(nums[i]>target)
+        {
+            return i;
+        }
+    } 
+    return numsSize;
+}
