90d887b72287f9ac0329f4c1694936e3697081c6
Ansh Thakur <ansh.27240@ggnindia.dronacharya.info>
2026-08-17 00:45:08 +0530

    Time: 17 ms (0.00%) | Memory: 18.1 MB (100.00%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.c
+++ 2156-stone-game-ix/stone-game-ix.c
@@ -1,21 +1,19 @@
 bool stoneGameIX(int* stones, int stonesSize) {
-    int count[3] = {0, 0, 0};
+   int count[3]={0,0,0};
 
-    // Count remainders
-    for (int i = 0; i < stonesSize; i++) {
-        count[stones[i] % 3]++;
-    }
+   for(int i=0;i<stonesSize;i++)
+   {
+    count[stones[i]%3]++;
+   }
+   if(count[0]%2==0)
+   {
+    return count[1]>0 && count[2]>0;
+   }
+   int diff=count[1]-count[2];
 
-    // Number of 0-remainder stones is even
-    if (count[0] % 2 == 0) {
-        return count[1] > 0 && count[2] > 0;
-    }
-
-    // Number of 0-remainder stones is odd
-    int diff = count[1] - count[2];
-
-    if (diff < 0)
-        diff = -diff;
-
-    return diff > 2;
+   if(diff<0)
+   {
+    diff=-diff;
+   }
+   return diff>2; 
 }
